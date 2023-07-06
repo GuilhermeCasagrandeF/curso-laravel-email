12a8fe30f74e34609d4715f3ca2dd5915acb26e2
GuilhermeCasagrandeF <guilherme.casagrande@soeltech.com.br>
2023-07-06 13:15:55 -0300

    Patch para assistir episódio

--- routes/api.php
+++ routes/api.php
@@ -25,3 +25,9 @@ Route::get('/series/{series}/seasons', function (\App\Models\Series $series){
 Route::get('/series/{series}/episodes', function (\App\Models\Series $series){
    return $series->episodes;
 });
+Route::patch('/episodes/{episode}', function (\App\Models\Episode $episode, Request $request){
+    $episode->watched = $request->watched;
+    $episode->save();
+
+   return $episode;
+});
